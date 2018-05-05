e05ec4fb7a6e4cabc82d5a5712baa95800c8ec40
bicliu <klins@163.com>
2018-05-05 11:11:49 +0800

    delete build files

--- build/Release/.deps/Release/multihashing.node.d
+++
@@ -1,1 +0,0 @@
-cmd_Release/multihashing.node := ln -f "Release/obj.target/multihashing.node" "Release/multihashing.node" 2>/dev/null || (rm -rf "Release/multihashing.node" && cp -af "Release/obj.target/multihashing.node" "Release/multihashing.node")
--- build/Release/.deps/Release/obj.target/multihashing/aesb.o.d
+++
@@ -1,3 +0,0 @@
-cmd_Release/obj.target/multihashing/aesb.o := cc '-DNODE_GYP_MODULE_NAME=multihashing' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/usr/include/nodejs/include/node -I/usr/include/nodejs/src -I/usr/include/nodejs/deps/uv/include -I/usr/include/nodejs/deps/v8/include -I../crypto -I../node_modules/nan  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -D_GNU_SOURCE -maes -fPIC -Ofast -flto -fuse-linker-plugin -funroll-loops -funswitch-loops -fpeel-loops -O3 -ffunction-sections -fdata-sections  -MMD -MF ./Release/.deps/Release/obj.target/multihashing/aesb.o.d.raw  -c -o Release/obj.target/multihashing/aesb.o ../aesb.c
-Release/obj.target/multihashing/aesb.o: ../aesb.c
-../aesb.c:
