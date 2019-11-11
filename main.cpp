c27a6467337495b215cdcb7c1e523d7dda9ea4ff
Tank <37199441+tkblack@users.noreply.github.com>
2019-11-11 15:26:05 +0800

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -18,9 +18,9 @@ int main(int argc, const char *argv[]) {
 	const char *mess = argv[1];
 	uint32_t messLen = (uint32_t)strlen(mess);
 
-	uint8_t input[INPUT_LEN];
-	memset(input, 0, INPUT_LEN*sizeof(uint8_t));
-	memcpy(input, mess, messLen*sizeof(char));
+	uint8_t input[messLen];
+	memset(input, 0, messLen*sizeof(uint8_t));
+	//memcpy(input, mess, messLen*sizeof(char));
 	
 	// Test for oneWayFunction
 	initOneWayFunction();
@@ -30,8 +30,8 @@ int main(int argc, const char *argv[]) {
 	
 	// powNistTest("./powNistTest");
 	
-	uint8_t Maddr[WORK_MEMORY_SIZE], output[OUTPUT_LEN];
-	memset(Maddr, 0, WORK_MEMORY_SIZE*sizeof(uint8_t));
+	//uint8_t Maddr[WORK_MEMORY_SIZE], output[OUTPUT_LEN];
+	//memset(Maddr, 0, WORK_MEMORY_SIZE*sizeof(uint8_t));
 
 	helloHash(input, messLen, output);
 	view_data_u8("PoW_char", output, OUTPUT_LEN);
